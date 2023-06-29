d959dd8d27d692270cabe6caea2a0038b4409161
Nicolas <nicolas.silva@utec.edu.uy>
2023-06-29 02:48:36 -0300

    si elimino una coleccion tambien la elimino en item

--- HobbyBox/vistas/tablas/eliminarColecciones.php
+++ HobbyBox/vistas/tablas/eliminarColecciones.php
@@ -5,8 +5,19 @@ $con = new Conexion();
 if (isset($_GET['id'])) {
     // Obtener el ID del usuario
     $idC = $_GET['id'];
+
+    // Obtener el nombre de la tabla
+    $sqlTabla = "SELECT nombre FROM coleccion WHERE idcoleccion = '$idC'";
+    $resultTabla = $con->ejecutarSQL($sqlTabla);
+    $rowTabla = mysqli_fetch_assoc($resultTabla);
+    $nombre = $rowTabla['nombre'];
+
     $sql = "delete from coleccion where idcoleccion='$idC'";
     $con->ejecutarSQL($sql);
+
+    $sql = "delete from item where coleccion='$nombre'";
+    $con->ejecutarSQL($sql);
+
     header("location: ../../indexAdmin.php");
 } else {
     // No se proporcionó un ID válido
