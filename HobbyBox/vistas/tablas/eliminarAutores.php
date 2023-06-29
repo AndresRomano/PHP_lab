6d854d8c01b0d083d8be1dc61dc395ae7b7fa812
Nicolas <nicolas.silva@utec.edu.uy>
2023-06-29 02:36:14 -0300

    ahora actualiza en la tabla item cuando borro un autor

--- HobbyBox/vistas/tablas/eliminarAutores.php
+++ HobbyBox/vistas/tablas/eliminarAutores.php
@@ -5,13 +5,29 @@ $con = new Conexion();
 if (isset($_GET['id'])) {
     // Obtener el ID del usuario
     $idA = $_GET['id'];
+
+    // Obtener el nombre de la tabla
+    $sqlTabla = "SELECT nombre FROM autores WHERE idAutor = '$idA'";
+    $resultTabla = $con->ejecutarSQL($sqlTabla);
+    $rowTabla = mysqli_fetch_assoc($resultTabla);
+    $nombre = $rowTabla['nombre'];
+
+
+
     $sql = "delete from autores where idAutor='$idA'";
     $con->ejecutarSQL($sql);
+
+    
+
+    $sql2 = "UPDATE item SET  autor=' ' WHERE autor = '$nombre'";            
+    $con->ejecutarSQL($sql2);
     header("location: ../../indexAdmin.php");
 } else {
     // No se proporcionó un ID válido
     // Manejar el error o redireccionar a una página de error
     header("location: ../../indexAdmin.php");
 }
+
+
 
 ?>
