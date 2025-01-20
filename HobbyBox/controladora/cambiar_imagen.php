fbc99bd4be81dd8e0deb3f55b6522e4d86a08c40
AndresRomano <93534475+AndresRomano@users.noreply.github.com>
2025-01-20 16:20:45 -0300

    Update cambiar_imagen.php
    bug arreglado

--- HobbyBox/controladora/cambiar_imagen.php
+++ HobbyBox/controladora/cambiar_imagen.php
@@ -1,28 +1,30 @@
 <?php
-    session_start();
-    include ("../persistencia/connect.php");
-    $con = new Conexion();
-    $nombrearchivo = $_FILES['fArchivo']['name'];
-    $rutaDestino ="../imagenes/";
-    $rutaArchivo = $rutaDestino.$nombrearchivo;
+session_start();
+include ("../persistencia/connect.php");
+$con = new Conexion();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Enviar'])) {
     $usuarioId = $_SESSION["idUser"];
+    $rutaDestino = "../imagenes/";
 
-     if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['Enviar'])) {       
-        // Verificar si se envió un archivo
-            if (!empty($_FILES['fArchivo']['name'])) {
-                $nombreArchivo = $_FILES['fArchivo']['name'];
-                $rutaArchivo = $rutaDestino.$nombrearchivo;
-        
-                // Mover el archivo a la ubicación deseada
-                if (move_uploaded_file($_FILES['fArchivo']['tmp_name'], $rutaArchivo)) {
-                    $sql = "UPDATE usuario SET imagen = '$nombrearchivo' AND idusuario = '$usuarioId'";
-                    $con->ejecutarSQL($sql);
-                    header("location: ../indexUser.php");
-                    exit;
-                } else {
-                    echo "Error al mover el archivo";
-                }
-            } 
+    if (!empty($_FILES['fArchivo']['name'])) {
+        $nombreArchivo = $_FILES['fArchivo']['name'];
+        $rutaArchivo = $rutaDestino . $nombreArchivo;
+
+        // Mover el archivo a la ubicación deseada
+        if (move_uploaded_file($_FILES['fArchivo']['tmp_name'], $rutaArchivo)) {
+            // Usar una consulta preparada para evitar inyección SQL
+            $sql = "UPDATE usuario SET imagen = '$nombreArchivo' WHERE idusuario = $usuarioId";
+            $con->ejecutarSQL($sql);
+            $_SESSION["foto"] = $nombreArchivo;
+            header("location: ../indexUser.php");
+            exit;
+        } else {
+            echo "Error al mover el archivo";
+        }
+    } else {
+        echo "No se seleccionó ningún archivo.";
     }
-
+}
+?>
     
